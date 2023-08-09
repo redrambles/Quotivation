9f88bbcefafb25918baa7a0d19e4b8bb1c86c9fb
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-08-09 17:16:36 -0400

    Favorite quotes part 1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState("All");
+  const [favoriteQuotes, setFavoriteQuotes] = useState([]);
+  const maxFaves = 3;
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
   const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
@@ -35,10 +37,31 @@ function App() {
 
   const filteredQuotes = category !== "All" ? quotes.filter((quote) => quote.categories.includes(category)) : quotes;
 
+  const addToFavorites = (quoteId) => {
+    const selectedQuote = quotes.find((quote) => quote.id === quoteId);
+
+    const alreadyFavorite = favoriteQuotes.find((favorite) => favorite.id === selectedQuote.id);
+
+    if (alreadyFavorite) {
+      console.log("You already favorited this quote!");
+    } else if (favoriteQuotes.length < maxFaves) {
+      setFavoriteQuotes([...favoriteQuotes, selectedQuote]);
+      console.log("Added to Favorites!");
+    } else {
+      console.log("Max number of quotes reached. Please delete one to add a new one.");
+    }
+  };
+
   return (
     <div className='App'>
       <Header />
       <main>
+        <section className='favorite-quotes'>
+          <div className='wrapper quotes'>
+            <h3>Top 3 favorite quotes</h3>
+            {favoriteQuotes.length > 0 && JSON.stringify(favoriteQuotes)}
+          </div>
+        </section>
         {loading ? (
           <Loader />
         ) : (
@@ -47,6 +70,7 @@ function App() {
             categories={categories}
             category={category}
             handleCategoryChange={handleCategoryChange}
+            addToFavorites={addToFavorites}
           />
         )}
       </main>
--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -2,16 +2,17 @@ import React from "react";
 import QuoteCard from "./QuoteCard";
 import CategoryForm from "./CategoryForm";
 
-const Quotes = ({ filteredQuotes, categories, category, handleCategoryChange }) => {
+const Quotes = ({ filteredQuotes, categories, category, handleCategoryChange, addToFavorites }) => {
   return (
     <section className='all-quotes'>
       <div className='quotes wrapper'>
         <div className='category-header'>
+          <h2 className='category-header'>Pick your Favorite Quotes Below</h2>
           <p>Browse through your collection of quotes</p>
           <CategoryForm categories={categories} category={category} handleCategoryChange={handleCategoryChange} />
         </div>
         {filteredQuotes.map((quote) => (
-          <QuoteCard key={quote.id} quote={quote} />
+          <QuoteCard key={quote.id} quote={quote} addToFavorites={addToFavorites} />
         ))}
       </div>
     </section>
