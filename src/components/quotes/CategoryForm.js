cb11c5f38adbefc309115161cd464ec3851e5f13
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-08-09 16:45:08 -0400

    Filter quotes by category

---
+++ src/components/quotes/CategoryForm.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Filter } from "react-feather";
+
+const CategoryForm = ({ categories, category, handleCategoryChange }) => {
+  return (
+    <div className='category-form'>
+      <form className='category-filter'>
+        <Filter />
+        <label htmlFor='category'>Filter Quotes:</label>
+        <select id='category' name='category' value={category} onChange={handleCategoryChange}>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </form>
+    </div>
+  );
+};
+
+export default CategoryForm;
